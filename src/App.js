762333b94a21a0b4c71ed0b9d348dc898a3b8c52
sana <sanabenhendalaajimi@gmail.com>
2019-10-31 00:23:32 +0100

    props checkout 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Warpper from "./Warpper";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+
+    <Photo src="/profile.png" />
+  
+    <Title style={{
+  
+      color:"red"
+  
+  }}>My Name here</Title>
+  
+    <Title small>My job here</Title>
+  
+  
+  </Wrapper>
+  )
 }
 
 export default App;
